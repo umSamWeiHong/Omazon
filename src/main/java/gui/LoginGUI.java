ce8650f3205a2d4d965f82afec17b2a0947b4021
samweihong <u2102776@siswa.um.edu.my>
2021-12-29 15:41:12 +0800

    Reconstruct project

---
+++ src/main/java/gui/LoginGUI.java
@@ -0,0 +1,22 @@
+package main.java.gui;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class LoginGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.RED);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
