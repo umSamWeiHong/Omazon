a2dfb244dad8d7eada7e1195c0359b7843ecdb0c
samweihong <weihongsam2002@gmail.com>
2021-12-31 13:37:39 +0800

    Added custom font and configurations

--- src/main/java/gui/MainGUI.java
+++ src/main/java/gui/MainGUI.java
@@ -4,9 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import main.java.Main;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainGUI extends Application {
@@ -21,17 +26,48 @@ public class MainGUI extends Application {
         stage.show();
     }
 
-    public void loginScene(Stage stage) throws IOException {
-        @SuppressWarnings("ConstantConditions")
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Scene scene = new Scene(root);
+    @SuppressWarnings("ConstantConditions")
+    public static void loadAllFonts() {
+        File directory = new File("src/main/resources/fonts/");
+        for (File file : directory.listFiles())
+            Font.loadFont(MainGUI.class.getClassLoader().getResourceAsStream("main/resources/fonts/" + file.getName()), 12);
+    }
 
-        stage.setScene(scene);
-        MainGUI.initStage(stage);
+    @SuppressWarnings("ConstantConditions")
+    public void loginScene(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene loginPage = new Scene(root);
+
+        stage.setScene(loginPage);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public void profileScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
+        System.out.println();
+        Image image = new Image("main/resources/img/icon.png");
+        System.out.println(image.getHeight());
+        String css = this.getClass().getResource("Profile.css").toExternalForm();
+        Scene profilePage = new Scene(root);
+
+        //Font.loadFont(MainGUI.class.getResourceAsStream("main/resources/fonts/Montserrat-Regular.otf"), 12);
+
+
+        profilePage.getStylesheets().add(css);
+
+        stage.setScene(profilePage);
+
+        Button button = (Button) profilePage.lookup("#middleButton");
+        button.setTextFill(Color.RED);
+
+        stage.setScene(profilePage);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        loginScene(stage);
+        MainGUI.loadAllFonts();
+        MainGUI.initStage(stage);
+        profileScene(stage);
     }
 }
