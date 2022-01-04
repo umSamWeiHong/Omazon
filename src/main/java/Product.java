ac51409f5e76fe9098f11d0ef0951b03ef72f75b
LEEKEATEN <u2102836@siswa.um.edu.my>
2022-01-04 23:39:24 +0800

    updated product and enum

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -3,19 +3,14 @@ package main.java;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Product {
+public class Product extends StoredDB {
 
-    private int productID;
-    private String productName;
-    private double price;
-    private int stock;
-    private int sales;
-    private double productRatings;
-    private String description;
+    private int productID, stock, sales;
+    private String productName, description;
+    private double price, productRatings;
     private Category category;
     private String[] reviews;
     private boolean bestSelling;
-    private boolean inDatabase;
 
     public Product() {
         productName = "";
@@ -31,16 +26,10 @@ public class Product {
     /** Create a Product object with data from database. */
     public Product(int productID) {
         String query = "SELECT * FROM Product WHERE productID = " + productID;
-        ResultSet resultSet = null;
-
-        try {
-            resultSet = Database.queryDatabase(query);
-            // Throw exception when productID is not found.
-            if (!resultSet.isBeforeFirst())
-                throw new IllegalArgumentException("ProductID is not found.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        ResultSet resultSet = Database.queryDatabase(query);
+        // Throw exception when productID is not found.
+        if (resultSet == null)
+            throw new IllegalArgumentException("ProductID is not found");
 
         try {
             resultSet.next();
@@ -51,9 +40,8 @@ public class Product {
             sales = resultSet.getInt("sales");
             productRatings = resultSet.getDouble("productRatings");
             description = resultSet.getString("description");
-            category = Category.valueOf(resultSet.getString("category"));
-
-            inDatabase = true;
+            category = Category.valueOf(Category.getEnum(resultSet.getString("category")));
+            setInDatabase(true);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -70,45 +58,6 @@ public class Product {
         this.productRatings = productRatings;
         this.description = description;
         this.category = Category.valueOf(category);
-        inDatabase = false;
-    }
-
-    /** Add this Product object to database. */
-    public void addToDatabase() {
-
-        // Do nothing if the review is already in database.
-        if (inDatabase) return;
-
-        String insertQuery = String.format("INSERT INTO " +
-                        "Product (productName, price, stock, sales, description, category) " +
-                        "VALUES ('%s', %f, %d, %d, '%s', '%s')",
-                productName, price, stock, sales, description, category);
-        Database.updateDatabase(insertQuery);
-        inDatabase = true;
-    }
-
-    /** Update this Product object in database. */
-    public void updateDatabase() {
-
-        // Do nothing if the review is not in database.
-        if (!inDatabase) return;
-
-        String updateQuery = String.format("UPDATE Product " +
-                        "SET productName = '%s', price = %f, stock = %d, sales = %d, description = '%s', category = '%s'" +
-                        "WHERE productID = %d",
-                productName, price, stock, sales, description, category);
-        Database.updateDatabase(updateQuery);
-    }
-
-    /** Delete this Product object in database. */
-    public void deleteFromDatabase() {
-
-        // Do nothing if the review is not in database.
-        if (!inDatabase) return;
-
-        String deleteQuery = "DELETE FROM Product " +
-                "WHERE productID = " + productID;
-        Database.updateDatabase(deleteQuery);
     }
 
     public Product(String productName, String description, double price, int stock, int sales, String[] reviews, String category) {
@@ -120,31 +69,40 @@ public class Product {
         this.reviews = reviews;
         this.category = Category.valueOf(category);
     }
+
     // Accessor
     public String getDescription() {
         return description;
     }
+
     public double getPrice() {
         return price;
     }
+
     public int getStock() {
         return stock;
     }
+
     public int getSales() {
         return sales;
     }
+
     public Double getProductRatings() {
         return productRatings;
     }
+
     public String[] getReviews() {
         return reviews;
     }
+
     public String getProductName() {
         return productName;
     }
+
     public Category getCategory() {
         return category;
     }
+
     public boolean isBestSelling() {
         return bestSelling;
     }
@@ -164,17 +122,8 @@ public class Product {
     public void setStock(int stock) {
         this.stock = stock;
     }
-    public void setSales(int sales) {
-        this.sales = sales;
-    }
-    public void setProductRatings(Double productRatings) {
-        this.productRatings = productRatings;
-    }
     public void setReviews(String[] reviews) {
         this.reviews = reviews;
-    }
-    public void setBestSelling(boolean bestSelling) {
-        this.bestSelling = bestSelling;
     }
 
     @Override
@@ -186,128 +135,73 @@ public class Product {
         details = details + "\nProduct ratings: " + productRatings;
         details = details + "\nDescription: " + description;
         details = details + "\nProduct reviews: " + reviews;
+
         return details;
     }
 
-    /** Method to get 3 best-selling products based on salescount*/
-    public static Product[] getBestSelling() {
+    /** Method to get 3 best-selling products based on salescount */
+    public static StoredDB[] getBestSelling() {
         String query = String.format("SELECT * FROM Product " +
-                        "ORDER BY sales DESC " +
-                        "LIMIT 3");
-
-        ResultSet resultSet = null;
-        try {
-            resultSet = Database.queryDatabase(query);
-            if (!resultSet.isBeforeFirst())
-                return null;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        Product[] product = new Product[3];
-        try {
-            int index = 0;
-            while (resultSet.next()) {
-                product[index] = new Product(resultSet.getInt("productID"));
-                index++;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return product;
+                "ORDER BY sales DESC " +
+                "LIMIT 3");
+        return Database.getDBObjects(query, Product.class, 3);
     }
 
     /** Method to get products by title */
-    public static Product[] getProductsByTitle(String title) {
+    public static StoredDB[] getProductsByTitle(String title) {
         String query = String.format("SELECT * FROM Product " +
-                "WHERE productName LIKE '%%%s%%'  ",
+                        "WHERE productName LIKE '%%%s%%'  ",
                 title);
 
-        ResultSet resultSet = null;
-        try {
-            resultSet = Database.queryDatabase(query);
-            if (!resultSet.isBeforeFirst())
-                return null;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        try {
-            // Initialize the array with number of results.
-            resultSet.last();
-            Product[] product = new Product[resultSet.getRow()];
-            resultSet.beforeFirst();
-
-            int index = 0;
-            while (resultSet.next()) {
-                product[index] = new Product(resultSet.getInt("productID"));
-                index++;
-            }
-            return product;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return Database.getDBObjects(query, Product.class, -1);
     }
 
     /** Method to get products by category */
-    public static Product[] getProductsByCategory(Category category) {
+    public static StoredDB[] getProductsByCategory(Category category) {
         String query = String.format("SELECT * FROM Product " +
-                "WHERE category = '%s'",
+                        "WHERE category = '%s'",
                 category);
 
-        ResultSet resultSet = null;
-        try {
-            resultSet = Database.queryDatabase(query);
-            if (!resultSet.isBeforeFirst())
-                return null;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            // Initialize the array with number of results.
-            resultSet.last();
-            Product[] product = new Product[resultSet.getRow()];
-            resultSet.beforeFirst();
-
-            int index = 0;
-            while (resultSet.next()) {
-                product[index] = new Product(resultSet.getInt("productID"));
-                index++;
-            }
-            return product;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return Database.getDBObjects(query, Product.class, -1);
     }
 
     public static void main(String[] args) {
-       /* Product product1 = new Product(0, "cake", 25.50, 100, 0, 0.0, "YUMMY");
-        try {
-            product1.addToDatabase();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        */
 
-       /*Product product[] = getBestSelling();
-      for(int i = 0; i < product.length;i++) {
-         System.out.println(product[i]);
+        /*StoredDB[] products = getBestSelling();
+        for (StoredDB i : getBestSelling())
+            System.out.println(i); */
 
-       } */
+        /*StoredDB[] products = getProductsByTitle("toothpaste");
+        for (StoredDB i : getProductsByTitle("toothpaste"))
+            System.out.println(i); */
 
-        /*Product product[] = getProductsByTitle("Face");
-        for(int i = 0; i < product.length;i++) {
-        System.out.println(product[i]);
+        StoredDB[] products = getProductsByCategory(Category.FASHION_ACCESSORIES);
+        for (StoredDB i : getProductsByCategory(Category.FASHION_ACCESSORIES))
+            System.out.println(i);
 
-        }*/
+    }
 
-        Product product[] = getProductsByCategory(Category.FASHION_ACCESSORIES);
-        for(int i = 0; i < product.length;i++) {
-            System.out.println(product[i]);
-        }
+    public static String getPrimaryKey() { return "productID"; }
+
+    @Override
+    public String insertQuery() {
+        return String.format("INSERT INTO " +
+                        "Product (productName, price, stock, sales, description, category) " +
+                        "VALUES ('%s', %f, %d, %d, '%s', '%s')",
+                productName, price, stock, sales, description, category);
+    }
+
+    @Override
+    public String updateQuery() {
+        return String.format("UPDATE Product " +
+                        "SET productName = '%s', price = %f, stock = %d, sales = %d, description = '%s', category = '%s'" +
+                        "WHERE productID = %d",
+                productName, price, stock, sales, description, category);
+    }
+
+    @Override
+    public String deleteQuery() {
+        return "DELETE FROM Product " +
+                "WHERE productID = " + productID;
     }
 }
