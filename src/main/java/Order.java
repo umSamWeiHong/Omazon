4340c65013b290fc45c01d9a92b496d219ae8d0e
damiafatihah <damiafatihah93@gmail.com>
2022-01-16 06:42:29 +0800

    updated to lastest version

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -3,11 +3,10 @@ package main.java;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Order {
+public class Order extends StoredDB {
 
     private int orderID, productID, customerID, sellerID;
     private String shippingAddress;
-    private boolean inDatabase;
 
     public Order() {
         orderID = 0;
@@ -17,19 +16,13 @@ public class Order {
         shippingAddress = "";
     }
 
-    /** Create a Order object with data from database. */
+    /** Create an Order object with data from database. */
     public Order(int orderID) {
         String query = "SELECT * FROM Order WHERE orderID = " + orderID;
-        ResultSet resultSet = null;
-
-        try {
-            resultSet = Driver.queryDatabase(query);
-            // Throw exception when productID is not found.
-            if (!resultSet.isBeforeFirst())
-                throw new IllegalArgumentException("OrderID is not found.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        ResultSet resultSet = Database.queryDatabase(query);
+        // Throw exception when orderID is not found.
+        if (resultSet == null)
+            throw new IllegalArgumentException("OrderID is not found.");
 
         try {
             resultSet.next();
@@ -38,20 +31,20 @@ public class Order {
             customerID = resultSet.getInt("customerID");
             sellerID = resultSet.getInt("sellerID");
             shippingAddress = resultSet.getString("shippingAddress");
-            inDatabase = true;
+            setInDatabase(true);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    /** Create a new Order object with all parameters (except reviewID). */
-    public Order(int productID, int customerID, int sellerID, String shippingAddress) {
+    /** Create a new Order object with all parameters */
+    public Order(int orderID, int productID, int customerID, int sellerID, String shippingAddress) {
+        this.orderID = orderID;
         this.productID = productID;
         this.customerID = customerID;
         this.sellerID = sellerID;
         this.shippingAddress = shippingAddress;
-        inDatabase = false;
     }
 
     // Accesor
@@ -67,52 +60,40 @@ public class Order {
     public void setSellerID(int sellerID) { this.sellerID = sellerID; }
     public void setShippingAddress(String shippingAddress) { this.shippingAddress = shippingAddress; }
 
-    /** Add this Order object to database. */
-    public void addToDatabase() throws SQLException {
+    public String toString() {
+        return "Order: " +
+                "\nOrder ID:" + orderID +
+                "\nProduct ID: " + productID +
+                "\nCustomer ID: " + customerID +
+                "Seller ID: " + sellerID +
+                "\nShipping Address:'" + shippingAddress +
+                "\n";
+    }
 
-        // Do nothing if the review is already in database.
-        if (inDatabase) return;
+    public String getPrimaryKey() {
+        return "orderID";
+    }
 
-        String insertQuery = String.format("INSERT INTO " +
+    @Override
+    public String insertQuery() {
+        return String.format("INSERT INTO " +
                         "Order (productID, customerID, sellerID, shippingAddress) " +
                         "VALUES ('%d', %d, %d, '%s')",
                 productID, customerID, sellerID, shippingAddress);
-        Driver.updateDatabase(insertQuery);
-        inDatabase = true;
     }
 
-    /** Update this Order object in database. */
-    public void updateDatabase() throws SQLException {
-
-        // Do nothing if the review is not in database.
-        if (!inDatabase) return;
-
-        String updateQuery = String.format("UPDATE Product " +
+    @Override
+    public String updateQuery() {
+        return String.format("UPDATE Product " +
                         "SET productID = '%d', customerID = %d, sellerID = %d, shippingAddress = '%s' " +
                         "WHERE orderID = %d",
                 productID, customerID, sellerID, shippingAddress);
-        Driver.updateDatabase(updateQuery);
     }
 
-    /** Delete this Order object in database. */
-    public void deleteFromDatabase() throws SQLException {
-
-        // Do nothing if the review is not in database.
-        if (!inDatabase) return;
-
-        String deleteQuery = "DELETE FROM Order " +
+    @Override
+    public String deleteQuery() {
+        return "DELETE FROM Order " +
                 "WHERE orderID = " + orderID;
-        Driver.updateDatabase(deleteQuery);
     }
 
-    public String toString() {
-        return "Order{" +
-                "orderID=" + orderID +
-                ", productID=" + productID +
-                ", customerID=" + customerID +
-                ", sellerID=" + sellerID +
-                ", shippingAddress='" + shippingAddress + '\'' +
-                ", inDatabase=" + inDatabase +
-                '}';
-    }
 }
